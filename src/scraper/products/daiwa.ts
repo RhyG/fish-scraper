045fdd1cd2539d363a4a2375838725aa7baa020e
rhyg <rhysgeary@gmail.com>
2024-02-18 18:24:53 +1000

    Daiwa rods done.

--- src/scraper/products/daiwa.ts
+++ src/scraper/products/daiwa.ts
@@ -14,25 +14,6 @@ interface Lure {
   length: string;
 }
 
-interface Reel {
-  model: string;
-  "ball bearing": string;
-  "drag pressure": string;
-  gear: string;
-  "spool capacity": string;
-  weight: string;
-  sku: string;
-  price: string;
-  drag: string;
-  bearings: string;
-  ratio: string;
-  "handle type": string;
-  "mono capacity": string;
-  "j-braid capacity": string;
-  "spool capacity (mono)": string;
-  "spool capacity (j-braid)": string;
-}
-
 const luresURL = "https://daiwafishing.com.au/collections/lures";
 const reelsURL = "https://daiwafishing.com.au/collections/reels";
 
@@ -151,6 +132,25 @@ async function scrapeLures() {
 //   }
 // });
 
+interface Reel {
+  model: string;
+  "ball bearing": string;
+  "drag pressure": string;
+  gear: string;
+  "spool capacity": string;
+  weight: string;
+  sku: string;
+  price: string;
+  drag: string;
+  bearings: string;
+  ratio: string;
+  "handle type": string;
+  "mono capacity": string;
+  "j-braid capacity": string;
+  "spool capacity (mono)": string;
+  "spool capacity (j-braid)": string;
+}
+
 async function scrapeReels() {
   const reels: Reel[] = [];
 
@@ -234,92 +234,215 @@ async function scrapeReels() {
   }
 }
 
-scrapeReels().then(async (reels) => {
-  console.log("WE GOT REELS BABY:", reels.length);
+// scrapeReels().then(async (reels) => {
+//   console.log("WE GOT REELS BABY:", reels.length);
 
+//   const start = performance.now();
+//   try {
+//     const sheet = FishDoc.sheetsByIndex[6];
+//     for await (const reel of reels) {
+// sheet.addRow({
+//   BRAND: "Daiwa",
+//   NAME: reel.model,
+//   "ITEM CODE": reel.sku,
+//   PRICE: reel.price,
+//   GEAR: reel.gear,
+//   WEIGHT: reel.weight,
+//   "BALL BEARING": reel["ball bearing"] ?? reel.bearings,
+//   DRAG: reel.drag ?? reel["drag pressure"],
+//   RATIO: reel.ratio,
+//   "HANDLE TYPE": reel["handle type"],
+//   "MONO CAPACITY": reel["mono capacity"] ?? reel["spool capacity (mono)"],
+//   "J-BRAID CAPACITY": reel["j-braid capacity"] ?? reel["spool capacity (j-braid)"],
+//   "SPOOL CAPACITY": reel["spool capacity"],
+// });
+// await new Promise((resolve) => setTimeout(resolve, 2000));
+//     }
+//   } catch (error) {
+//     console.log(error);
+//   } finally {
+//     const end = performance.now();
+//     console.log("Time taken:", end - start);
+//   }
+// });
+
+interface Rod {
+  Model: string;
+  action: string;
+  castweight: string;
+  length: string;
+  "Rec. Line": string;
+  section: string;
+  taper: string;
+  type: string;
+  sku: string;
+  price: string;
+}
+
+async function scrapeRods() {
+  const rodsURL = "https://daiwafishing.com.au/collections/rods";
+
+  const rods: Rod[] = [];
+
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+
+  try {
+    page.on("console", (msg) => console.log("PAGE LOG:", msg.text()));
+
+    await page.goto(rodsURL, { waitUntil: "domcontentloaded" });
+    await page.setViewport({ width: 1080, height: 1024 });
+
+    const products = await page.evaluate(async () => {
+      await new Promise((resolve) => {
+        var totalHeight = 0;
+        var distance = 1000; // Should be less than or equal to window.innerHeight
+        var timer = setInterval(() => {
+          var scrollHeight = document.body.scrollHeight;
+          window.scrollBy(0, distance);
+          totalHeight += distance;
+
+          if (totalHeight >= scrollHeight) {
+            clearInterval(timer);
+            resolve("");
+          }
+        }, 2000);
+      });
+
+      const selector = ".grid.grid-border";
+      const parent = document.querySelector(selector);
+      if (!parent) return [];
+
+      // Select all div children directly under the parent
+      // Then find the anchor tag inside each div and get its href attribute
+      // @ts-ignore
+      const arr = Array.from(parent.querySelectorAll("div > a")).map((a) => a.href);
+      return Array.from(new Set(arr.filter((a) => a !== "" && a.includes("https"))));
+    });
+
+    console.log("Got products:", products.length);
+
+    for (const product of products) {
+      // Open the product page
+      const detailPage = await browser.newPage();
+      detailPage.on("console", (msg) => console.log("DETAIL PAGE LOG:", msg.text()));
+      await detailPage.goto(product, { waitUntil: "domcontentloaded" });
+
+      const extractedData = await detailPage.evaluate(() => {
+        const allRowsData: any[] = [];
+
+        // Find all tables within the container
+        const tables = Array.from(document.querySelectorAll(".dataTable"));
+
+        tables.forEach((table) => {
+          // Extract headers text, avoiding child elements text (like those in dropdowns)
+          const headers = Array.from(table.querySelectorAll("th")).map((th) => {
+            // Attempt to directly use textContent of a child <p> or the th itself if <p> is not found
+            // @ts-ignore
+            const headerText = th.querySelector("p") ? th.querySelector("p").textContent.trim() : th.textContent.trim();
+            // console.log("HEADER TEXT", headerText);
+            return headerText.replace(/\s+/g, " "); // Normalize whitespace
+          });
+
+          // console.log("HEADERS", headers);
+
+          // Iterate over each row in the table
+          const rows = Array.from(table.querySelectorAll("tbody tr"));
+          rows.forEach((row) => {
+            const rowData = {};
+            const cells = Array.from(row.querySelectorAll("td"));
+
+            // Map each cell to its corresponding header
+            cells.forEach((cell, index) => {
+              // console.log("CELL", cell.innerText.trim());
+              if (typeof headers[index] === "string")
+                // @ts-ignore
+                rowData[headers[index] || `unknown${index}`] = cell.innerText.trim();
+            });
+
+            // Add the constructed object to the allRowsData array
+            allRowsData.push(rowData);
+          });
+        });
+
+        return allRowsData;
+      });
+      rods.push(...extractedData);
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    browser.close();
+    return rods;
+  }
+}
+
+scrapeRods().then(async (rods) => {
   const start = performance.now();
   try {
-    const sheet = FishDoc.sheetsByIndex[6];
-    for await (const reel of reels) {
+    console.log("WE GOT RODS BABY:", rods.length);
+    const filteredRods = rods.filter((rod) => Object.keys(rod).length > 0);
+
+    const sheet = FishDoc.sheetsByIndex[7];
+    for await (const rod of filteredRods) {
       sheet.addRow({
         BRAND: "Daiwa",
-        NAME: reel.model,
-        "ITEM CODE": reel.sku,
-        PRICE: reel.price,
-        GEAR: reel.gear,
-        WEIGHT: reel.weight,
-        "BALL BEARING": reel["ball bearing"] ?? reel.bearings,
-        DRAG: reel.drag ?? reel["drag pressure"],
-        RATIO: reel.ratio,
-        "HANDLE TYPE": reel["handle type"],
-        "MONO CAPACITY": reel["mono capacity"] ?? reel["spool capacity (mono)"],
-        "J-BRAID CAPACITY": reel["j-braid capacity"] ?? reel["spool capacity (j-braid)"],
-        "SPOOL CAPACITY": reel["spool capacity"],
+        NAME: rod.Model,
+        "ITEM CODE": rod.sku,
+        PRICE: rod.price,
+        ACTION: rod.action,
+        CASTWEIGHT: rod.castweight,
+        LENGTH: rod.length,
+        "REC. LINE": rod["Rec. Line"],
+        SECTION: rod.section,
+        TAPER: rod.taper,
+        TYPE: rod.type,
       });
       await new Promise((resolve) => setTimeout(resolve, 2000));
     }
   } catch (error) {
-    console.log(error);
+    console.log("ERROR:", error);
   } finally {
     const end = performance.now();
     console.log("Time taken:", end - start);
   }
 });
 
-// const x = {
-//   model: "MARINEPOWER 3000 (12V)",
-//   "ball bearing": "16BB (2CRBB-1MSBB)",
-//   "drag pressure": "90kg",
-//   gear: "1.7",
-//   "spool capacity": "PE 1400/120lb; 1000/150lb",
-//   weight: "5560g",
-//   sku: "26866",
-//   price: "$5899.00",
-// };
-// const z = {
-//   model: "23 MAVERICK 6000-XH",
-//   gear: "6.2",
-//   weight: "430g",
-//   "ball bearing": "7BB (1 CRBB, 1MSBB, 1RB)",
-//   drag: "12kg",
-//   "spool capacity": "PE 3/300m",
-//   "spool capacity (j-braid)": "PE 2/ 300m",
-//   "spool capacity (mono)": "20lb- 150m",
-//   sku: "22141",
-//   price: "$599.00",
-//   ratio: "6.2:1",
-// };
-
-// const y = {
-//   model: "23 TD SOL HD 3000D-XH",
-//   gear: "6.2:1 (93cm)",
-//   weight: "255g",
-//   bearings: "8+1 (5 CRBB)",
-//   "drag pressure": "10kg",
-//   "spool capacity": "PE 1.5/300m",
-//   sku: "21716",
-//   price: "$409.00",
-// };
-
-// const f = {
-//   model: "16 SALTIST 6500",
-//   "ball bearing": "7CRBB +1MJ; 1RB",
-//   "drag pressure": "15kg",
-//   gear: "5.3",
-//   "spool capacity": "PE 5/500m; 6/400m",
-//   weight: "850g",
-//   sku: "22246",
-//   price: "$339.00",
-// };
-
-// const a = {
-//   model: "AGGREST LT 5000 SPIN REEL",
-//   "ball bearing": "4BB; 1RB",
-//   "drag pressure": "12kg",
-//   gear: "5.2",
-//   "spool capacity": "PE 1.5/200m",
-//   weight: "280g",
-//   sku: "21473",
-//   price: "$0.00",
-//   "handle type": "Soft Touch",
-// };
+const arr = [
+  {
+    Model: "BEEFSTICK 1403XXHS SURF ROD",
+    action: "XX Heavy",
+    castweight: "N/A",
+    length: `14'0"/427cm`,
+    "Rec. Line": "15 - 24 kg",
+    section: "3",
+    taper: "Reg",
+    type: "Spin",
+    sku: "12622",
+    price: "$99.99",
+  },
+  {
+    Model: "BEEF STICK 701HB",
+    action: "Heavy",
+    castweight: "N/A",
+    length: `7'0"/213cm`,
+    "Rec. Line": "6-10kg",
+    section: "1",
+    taper: "Reg",
+    type: "Overhead",
+    sku: "12644",
+    price: "$69.99",
+  },
+  {
+    Model: "21 SENSOR SANDSTORM 1062M",
+    action: "Medium",
+    castweight: "10-60g",
+    length: `10'6"/320cm`,
+    "Rec. Line": "6-12kg",
+    section: "2",
+    taper: "Fast",
+    type: "Spin",
+    sku: "11028",
+    price: "$249.00",
+  },
+];
